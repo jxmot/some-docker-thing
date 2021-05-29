69179441b331adff768a999e197c4af8ae1a372c
jxmot <webdev@stupiddog.net>
2021-05-29 04:48:46 -0500

    initial commit, rules storage with canned rules

---
+++ rules.js
@@ -0,0 +1,155 @@
+'use strict';
+module.exports = {
+    sensors: {
+        '{87c89411-55f7-4cab-9b54-6d0895b2bafc}': {
+            enable:true,
+            checks: {
+                'fahrenheit': {
+                    trigger:90.0,
+                    delta:0.0,
+                    check:'GLT'
+                },
+                'celsius': {
+                    trigger:32.2,
+                    delta:0.0,
+                    check:'GLT'
+                }
+            }
+        },
+        '{b6d920d5-382b-4b32-a7e9-0e906d9ccd38}': {
+            enable:true,
+            checks: {
+                'fahrenheit': {
+                    trigger:90.0,
+                    delta:0.0,
+                    check:'GT'
+                },
+                'celsius': {
+                    trigger:32.2,
+                    delta:0.0,
+                    check:'GT'
+                }
+            }
+        },
+        '{10980385-ae81-48c7-ba07-bf94b30108fa}': {
+            enable:true,
+            checks: {
+                'fahrenheit': {
+                    trigger:90.0,
+                    delta:0.0,
+                    check:'GT'
+                },
+                'celsius': {
+                    trigger:32.2,
+                    delta:0.0,
+                    check:'GT'
+                }
+            }
+        },
+        '{d420def6-7c8e-4301-b1a9-da9fe76fa034}': {
+            enable:true,
+            checks: {
+                'fahrenheit': {
+                    trigger:90.0,
+                    delta:0.0,
+                    check:'GT'
+                },
+                'celsius': {
+                    trigger:32.2,
+                    delta:0.0,
+                    check:'GT'
+                }
+            }
+        },
+        '{9ee6be3e-7a80-47ed-aba4-20d2d5b45474}': {
+            enable:true,
+            checks: {
+                'fahrenheit': {
+                    trigger:90.0,
+                    delta:0.0,
+                    check:'GT'
+                },
+                'celsius': {
+                    trigger:32.2,
+                    delta:0.0,
+                    check:'GT'
+                }
+            }
+        },
+        '{c2270309-cd55-4c83-83a0-7960dacd03bc}': {
+            enable:true,
+            checks: {
+                'fahrenheit': {
+                    trigger:90.0,
+                    delta:0.0,
+                    check:'GT'
+                },
+                'celsius': {
+                    trigger:32.2,
+                    delta:0.0,
+                    check:'GT'
+                }
+            }
+        },
+        '{5fbf6c4b-ca24-42dc-a161-2e3d188f8420}': {
+            enable:true,
+            checks: {
+                'fahrenheit': {
+                    trigger:90.0,
+                    delta:0.0,
+                    check:'GT'
+                },
+                'celsius': {
+                    trigger:32.2,
+                    delta:0.0,
+                    check:'GT'
+                }
+            }
+        },
+        '{edb8f578-7d85-429c-a4ce-7cca1cf1cde3}': {
+            enable:true,
+            checks: {
+                'fahrenheit': {
+                    trigger:90.0,
+                    delta:0.0,
+                    check:'GT'
+                },
+                'celsius': {
+                    trigger:32.2,
+                    delta:0.0,
+                    check:'GT'
+                }
+            }
+        },
+        '{d9ab9140-d1e9-4bed-97c9-980bce943b63}': {
+            enable:true,
+            checks: {
+                'fahrenheit': {
+                    trigger:90.0,
+                    delta:0.0,
+                    check:'GT'
+                },
+                'celsius': {
+                    trigger:32.2,
+                    delta:0.0,
+                    check:'GT'
+                }
+            }
+        },
+        '{da272a5c-a645-4738-b763-5585e65aa27d}': {
+            enable:true,
+            checks: {
+                'fahrenheit': {
+                    trigger:90.0,
+                    delta:0.0,
+                    check:'GT'
+                },
+                'celsius': {
+                    trigger:32.2,
+                    delta:0.0,
+                    check:'GT'
+                }
+            }
+        }
+    }
+};
