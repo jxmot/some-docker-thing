837d20215d97a496cb8ea094f178d2821ac1099b
jxmot <webdev@stupiddog.net>
2021-05-29 05:08:11 -0500

    comments

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 'use strict';
+/*
+    https://github.com/jxmot/some-docker-thing
 
+    The best way to test this application (once it is 
+    up and running) is to use something like Postman.
+
+    Postman works very well for testing the APIs. Here 
+    is some test data to use with it (in the POST 
+    body) - 
+
+    Sensor Data:
+        {"id": "{87c89411-55f7-4cab-9b54-6d0895b2bafc}","value": 89,"unit": "fahrenheit"}
+
+    Rule Data:
+        {"id": "{87c89411-55f7-4cab-9b54-6d0895b2bafc}","enable": true,"trigger": 70.6,"unit": "fahrenheit","delta": 0.0,"check": "GT"}
+
+    NOTE: "id" strings can be found in data-samples.txt
+*/
 // Events
 const EventEmitter = require('events');
 // incoming api events are sent here...
